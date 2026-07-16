8ee8c03bd571943096418859f8f0902cc74ebae6
Thomas Eduardo <developer.thomas@outlook.com.br>
2026-07-15 23:53:13 -0300

    feat: add Google Tag Manager (GTM-T9K46BCS) to root layout for all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -137,8 +137,29 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T9K46BCS');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body className="font-sans font-light antialiased overflow-x-hidden" suppressHydrationWarning={true}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T9K46BCS"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <SmoothScroll>
           <PageLoader />
           <CustomCursor />
